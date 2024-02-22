adbdbc4a662ab8e14708c7a6c23b05ebec189b23
carlosmaycon <155014371+carlosmaycon@users.noreply.github.com>
2024-02-22 17:17:46 -0300

    atualização

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,41 @@
-document.addEventListener('click', (e) => {
-  const el = e.target
+document.addEventListener('click', (event) => {
+    const el = event.target
 
-  const menu = document.querySelector('#menu')
-  const styleEl = getComputedStyle(menu)
+    if (el.classList.contains('line') || el.classList.contains('menuIco') || el.classList.contains('hamburger'))
+        showMenu()
 
-  if (el.classList.contains('line') || el.classList.contains('menuIco') || el.classList.contains('hamburger')) {
+    if (el.classList.contains('btn'))
+        localSelect(event)
+})
+
+function showMenu() {
+    const menu = document.querySelector('#menu')
+    const styleEl = getComputedStyle(menu)
+
     const tableIcoTd = document.querySelector('#td-menu')
     const containerIcoSpan = document.querySelector('.menuIcoContainer')
     const containMenuIco2 = document.querySelector('#menuIco2')
 
     if (styleEl.display === 'none') {
-      menu.style.display = 'block'
-      containMenuIco2.appendChild(containerIcoSpan)
+        menu.style.display = 'block'
+        containMenuIco2.appendChild(containerIcoSpan)
 
     } else {
-      tableIcoTd.appendChild(containerIcoSpan)
-      menu.style.display = 'none'
+        tableIcoTd.appendChild(containerIcoSpan)
+        menu.style.display = 'none'
     }
-  }
-})
+}
+
+function localSelect(e) {
+    e.preventDefault()
+
+    const inputCity = document.querySelector('#inp-local').value
+
+    captureWeather(inputCity)
+}
+
+function captureWeather(citySelect) {
+    const city = citySelect.toLowerCase()
+    const kay = '93a12f36a8cc9e0a562b1aa29fd8955b'
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apiKey}&units=metric'
+}
