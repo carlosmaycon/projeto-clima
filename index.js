dd5ca14fdfa6d5274b672bb819fb040b55f51ca7
carlosmaycon <155014371+carlosmaycon@users.noreply.github.com>
2024-02-22 12:53:01 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+document.addEventListener('click', (e) => {
+  const el = e.target
+
+  const menu = document.querySelector('#menu')
+  const styleEl = getComputedStyle(menu)
+
+  if (el.classList.contains('line') || el.classList.contains('menuIco') || el.classList.contains('hamburger')) {
+    const tableIcoTd = document.querySelector('#td-menu')
+    const containerIcoSpan = document.querySelector('.menuIcoContainer')
+    const containMenuIco2 = document.querySelector('#menuIco2')
+
+    if (styleEl.display === 'none') {
+      menu.style.display = 'block'
+      containMenuIco2.appendChild(containerIcoSpan)
+      
+    } else {
+      tableIcoTd.appendChild(containerIcoSpan)
+      menu.style.display = 'none'
+    }
+  }
+})
